77cca330c8e4d4a541c551d76809b00d5c463d78
Ryan Dufrene <rdufrene@mitre.org>
2018-05-15 14:44:02 -0400

    Basic stuff to get random dice on pages.

--- Makefile
+++ Makefile
@@ -1,13 +1,33 @@
-GO_BUILD_ENV := CGO_ENABLED=0 GOOS=linux GOARCH=amd64
-DOCKER_BUILD=$(shell pwd)/.docker_build
-DOCKER_CMD=$(DOCKER_BUILD)/dice-roller
+		# Go parameters
+    GOCMD=go
+    GOFMT=$(GOCMD) fmt
+    GOBINFLDR=bin
+    GOBUILD=$(GOCMD) build
+    GOCLEAN=$(GOCMD) clean
+    GOTEST=$(GOCMD) test
+    GOGET=$(GOCMD) get
+    BINARY_NAME=dice-roller-binary
+    BINARY_UNIX=$(BINARY_NAME)_unix
 
-$(DOCKER_CMD): clean
-	mkdir -p $(DOCKER_BUILD)
-	$(GO_BUILD_ENV) go build -v -o $(DOCKER_CMD) .
-
-clean:
-	rm -rf $(DOCKER_BUILD)
-
-heroku: $(DOCKER_CMD)
-	heroku container:push web
+    all: fmt lint test build
+    clean:
+						$(GOCLEAN)
+						rm -f $(BINARY_NAME)
+						rm -f $(BINARY_UNIX)
+    fmt:
+						$(GOFMT)
+    lint:
+						$(GOGET) github.com/golang/lint/golint
+						golint
+    test:
+						$(GOTEST) -v ./...
+    build:
+						$(GOBUILD) -o $(BINARY_NAME) -v $(GOBINFLDR)
+    run:
+						$(GOBUILD) -o $(BINARY_NAME) -v ./...
+						./$(BINARY_NAME)
+    # Cross compilation
+    build-linux:
+            CGO_ENABLED=0 GOOS=linux GOARCH=amd64 $(GOBUILD) -o $(BINARY_UNIX) -v
+    docker-build:
+            #docker run --rm -it -v "$(GOPATH)":/go -w /go/src/bitbucket.org/rsohlich/makepost golang:latest go build -o "$(BINARY_UNIX)" -v
--- main.go
+++ main.go
@@ -1,16 +1,68 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+  "fmt"
+  "log"
+  "strconv"
+  "strings"
+  "math/rand"
+  "os"
+  // "html/template"
+  // "io/ioutil"
+  "net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Pick your dice!", r.URL.Path[1:])
+}
+
+func commonSetHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "This is the common set of dice!", r.URL.Path[1:])
+}
+
+func customSetHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Make your own set of dice!")
+}
+
+func randomPageHandler(w http.ResponseWriter, r *http.Request) {
+  // If it is a number simulate a dice with that many sides
+  if _, err := strconv.Atoi(r.URL.Path[1:]); err == nil {
+    fmt.Fprintf(w, "You rolled a D%s! \n", r.URL.Path[1:])
+    inputInt, err := strconv.Atoi(r.URL.Path[1:])
+    if err != nil {
+        // handle error
+        fmt.Println(err)
+        os.Exit(2)
+    }
+    fmt.Fprintf(w, "it was a %d", randomValue(1, inputInt))
+  } else if strings.HasSuffix(r.URL.Path[1:], ".html") {
+    if strings.HasPrefix(r.URL.Path[1:], "static/") {
+      http.ServeFile(w, r, r.URL.Path[1:])
+    } else {
+      http.ServeFile(w, r, "static/" + r.URL.Path[1:])
+    }
+  } else {
+    fmt.Fprintf(w, "Sorry but it seems this page does not exist...")
+  }
+}
+
+// Gets a random value from the low to high values. This will include the low and high values.
+func randomValue(low int, high int) int {
+  var scaledInt int = high - low + 1  // The +1 is to offset the values so it can be the high value.
+  return rand.Intn(scaledInt) + low
+}
+
+// Basic Dice object.
+// D20 would have a High of 20 and Low of 1.
+type Dice struct {
+    High int
+    Low  int
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+  // http.HandleFunc(nil, homeHandler)
+  http.HandleFunc("/common/", commonSetHandler)
+  http.HandleFunc("/custom/", customSetHandler)
+  http.HandleFunc("/", randomPageHandler)
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
